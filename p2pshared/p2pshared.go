5fe133e7046f3e847d5554f0e575687afca8fca9
turtlebasket <turtlebasket@gmail.com>
2021-12-08 22:08:22 -0800

    batch register messages

--- p2pshared/p2pshared.go
+++ p2pshared/p2pshared.go
@@ -1,7 +1,26 @@
 package p2pshared
 
+import (
+	"x-bootstrap-node/xutil"
+
+	"github.com/perlin-network/noise"
+)
+
 var BootstrapNodes = [...]string{
 	"143.198.176.143", // DigitalOcean host
 	// "10.142.89.192",   // rpi on LAN
 	// "10.142.26.69", // thinkpad on LAN
+}
+
+func RegisterNodeMessages(n *noise.Node) error {
+
+	n.RegisterMessage(xutil.PeerInfo{}, xutil.UnmarshalPeerInfo)
+
+	n.RegisterMessage(xutil.PaymentIntent{}, xutil.UnmarshalPaymentIntent)
+	n.RegisterMessage(xutil.PaymentResponse{}, xutil.UnmarshalPaymentResponse)
+
+	n.RegisterMessage(xutil.PaymentCancellation{}, xutil.UnmarshalPaymentCancellation)
+	n.RegisterMessage(xutil.PaymentCancellationResponse{}, xutil.UnmarshalPaymentCancellationResponse)
+
+	return nil
 }
